f866f01bc18c2c465996272616cc0feb9400df45
Stiven <zono-capensis@protonmail.com>
2022-09-18 15:55:14 -0500

    Put all classes in one file.

--- data/Node.py
+++
@@ -1,6 +0,0 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-    def get_value(self):
-        return self.value
--- data/structures.py
+++ data/structures.py
@@ -1,3 +1,10 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    def get_value(self):
+        return self.value
+
 class Stack:
     def __init__(self):
         self.head = Node("Head")
