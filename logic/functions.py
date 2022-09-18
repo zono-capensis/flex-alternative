78560a46bf45a9b6532f34cb934e7dcfe80217a0
Stiven <zono-capensis@protonmail.com>
2022-09-18 16:31:45 -0500

    Create files for functions.

---
+++ logic/functions.py
@@ -0,0 +1,41 @@
+import string
+from Stack import Stack
+
+def infix_to_postfix(regex, alphabet, operations):
+    operation_order = Stack();
+    post_string = ""
+    for i in range(len(regex)):
+        if regex[i] in alphabet:
+            post_string += regex[i]
+        elif regex[i] in operations:
+            while (not operation_order.is_empty() and has_higher_precedence(operation_order.peek(), regex[i]) and operation_order.peek()!='('):
+                post_string += operation_order.pop()
+            operation_order.push(regex[i])
+        elif regex[i]=='(':
+            operation_order.push('(')
+        elif regex[i]==')':
+            while (not operation_order.is_empty() and operation_order.peek()!='('):
+                post_string += operation_order.pop()
+            operation_order.pop()
+
+    while (not operation_order.is_empty()):
+        post_string += operation_order.pop()
+    return post_string
+
+def has_higher_precedence(first_operator, second_operator):
+    return (first_operator < second_operator)
+
+#Change alphabet
+def add_concatenation_operation(regex):
+    temp_regex = list(regex)
+    new_regex = ""
+    for i in range(len(temp_regex)-1):
+        if (temp_regex[i] in string.ascii_lowercase and (temp_regex[i+1] in string.ascii_lowercase or temp_regex[i+1] == '(')):
+            new_regex = new_regex + temp_regex[i] + ".";
+
+        elif ((temp_regex[i]==')' or temp_regex[i]=='*') and temp_regex[i+1] in string.ascii_lowercase):
+            new_regex = new_regex + temp_regex[i] + ".";
+        else:
+            new_regex += temp_regex[i]
+    new_regex += temp_regex[-1]
+    return new_regex
