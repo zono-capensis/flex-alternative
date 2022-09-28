2c6ec02fd9cf12e64d00693a8dc20ac10b8b399d
Juan Sanchez <jusanchez@unal.edu.co>
2022-09-27 21:21:19 -0500

    nfa to dfa working

--- logic/test.py
+++ logic/test.py
@@ -5,13 +5,13 @@ from structures import Finite_automata
 import nfaToDfa
 nfa = Finite_automata(["a", "b"], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0, [10],
     {
-        0: {"eps":[1,7]},
-        1: {"eps":[2,4]},
+        0: {"":[1,7]},
+        1: {"":[2,4]},
         2: {"a":[3]},
-        3: {"eps":[6]},
+        3: {"":[6]},
         4: {"b":[5]},
-        5: {"eps":[6]},
-        6: {"eps":[1,7]},
+        5: {"":[6]},
+        6: {"":[1,7]},
         7: {"a":[8]},
         8: {"b":[9]},
         9: {"b":[10]},
@@ -19,4 +19,4 @@ nfa = Finite_automata(["a", "b"], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0, [10],
      }
 )
 trans = nfaToDfa.SubsetConstruction(nfa)
-print(trans.calculeEpsClosure([3,8]))
+print(trans.calculeSubsetConstruction())
